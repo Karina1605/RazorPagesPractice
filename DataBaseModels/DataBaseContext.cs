e93619f8e33ea4b0d6e7edac5cd39cf50f65fe11
Karina1605 <akchurinakarina1605@yandex.ru>
2021-08-29 21:51:28 +0300

    initial commit

---
+++ DataBaseModels/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPages.DataBaseModels
+{
+    public class DataBaseContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=SocialNetwork;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ DataBaseModels/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPages.DataBaseModels
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public float Cost { get; set; }
+        [Required]
+        public int Count { get; set; }
+        public string FilePath { get; set; }
+    }
+}
